017edee293f53da89696470c95b6466e9c80ab7d
Rajab Sahebkhan <sahebkhanrajab@gmail.com>
2026-07-21 17:12:21 +0200

    Add Array, ArrayList, and HashMap examples

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 10;
+        arr[1] = 3;
+
+        int[] nums = {1, 3, 6, 7};
+
+        for(int i = 0; i< nums.length; i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
